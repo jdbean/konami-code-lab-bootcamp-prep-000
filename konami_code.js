88b5f4cac5a2f88fc3d9fffa71697577558ac2f5
J.D. Bean <jonathandbean@gmail.com>
2017-12-06 20:16:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,22 +4,23 @@ const body = document.body
 
 let index = 0;
 
-function init(e) {
-  const key = parseInt(e.detail || e.which);
-  console.log(e.which);
-  if (key === code[index]) {
-    index++;
+function init() {
+  function test(e) {}
+    const key = parseInt(e.detail || e.which);
+    console.log(e.which);
+    if (key === code[index]) {
+      index++;
 
-    if (index === code.length) {
-      alert("Way to go; you have been granted infinite lives!");
+      if (index === code.length) {
+        alert("Way to go; you have been granted infinite lives!");
 
+        index = 0;
+      }
+
+    } else {
       index = 0;
     }
-
-  } else {
-    index = 0;
-  }
-
+  body.addEventListener('keydown', test);
 }
 
-body.addEventListener('keydown', init);
+
