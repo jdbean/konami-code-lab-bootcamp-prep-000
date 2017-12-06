b9bceb5d1ea92fa89f7145d4e284ff28e8196a42
J.D. Bean <jonathandbean@gmail.com>
2017-12-06 19:18:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let index = 0;
 
-input.addEventListener('keydown', init(e)) {
+input.addEventListener('keydown', init(e) {
   console.log(e.which)
 })
 
