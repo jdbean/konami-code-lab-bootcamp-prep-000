08b4ba2d2fbec91e7e782d9b78d412f86fb16938
J.D. Bean <jonathandbean@gmail.com>
2017-12-06 19:34:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,8 @@ const body = document.body
 
 let index = 0;
 
-function init(e) {
+
+body.addEventListener('keydown', function init(e) {
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
@@ -19,6 +20,4 @@ function init(e) {
     index = 0;
   }
 
-}
-
-body.addEventListener('keydown', init(e))
+})
